267000ba25c1e1b42793bf794225084b9a503f1c
Aiken <aiken.ong@u.nus.edu>
2026-09-16 22:16:44 +0800

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] res = new int[nums.length];
+
+        int prefix = 1;
+
+        res[0] = prefix;
+
+        for (int i = 1; i < nums.length; i++) {
+            prefix *= nums[i - 1];
+
+            res[i] = prefix;
+        }
+
+        int suffix = 1;
+
+        res[nums.length - 1] *= suffix;
+
+        for (int i = nums.length - 2; i >= 0; i--) {
+            
+            suffix *= nums[i + 1];
+        
+            res[i] *= suffix; 
+        }
+
+        return res;
+    }
+}  
